73f9bddbdffe0b86510f01871affbf47454ec380
Yaroslav Lozhkin <yalozhkin@gmail.com>
2023-10-27 12:07:31 +0400

    Improved copy

--- code.ts
+++ code.ts
@@ -3,40 +3,34 @@
 function swapInstances(): void {
   // Check if there are at least two nodes selected
   if (figma.currentPage.selection.length < 2) {
-    figma.closePlugin('Please select at least one nested instance and one content instance.');
+    figma.closePlugin('✋ Select two or more instances. The latest selected will replace the others.');
     return;
   }
 
-  let contentInstance: InstanceNode | null = null;
-  const nestedInstances: InstanceNode[] = [];
+  let targetInstance: InstanceNode | null = null;
+  const swapInstances: InstanceNode[] = [];
 
-  // Identify the content instance and collect all nested instances
+  // Identify the target instance and collect all swap instances
   for (const node of figma.currentPage.selection) {
     if (node.type === 'INSTANCE' && (!node.parent || node.parent.type !== 'INSTANCE')) {
-      contentInstance = node;
+      targetInstance = node as InstanceNode;
     } else if (node.type === 'INSTANCE') {
-      nestedInstances.push(node);
+      swapInstances.push(node as InstanceNode);
     }
   }
 
-  // Check if a content instance was identified and there's at least one nested instance
-  if (!contentInstance || nestedInstances.length === 0) {
-    figma.closePlugin('Please select at least one nested instance and one content instance.');
+  // Check if a target instance was identified and there's at least one swap instance
+  if (!targetInstance || swapInstances.length === 0) {
+    figma.closePlugin('✋ Select two or more instances. The latest selected will replace the others.');
     return;
   }
 
-  // Check if the content instance has a main component
-  if (!contentInstance.mainComponent) {
-    figma.closePlugin('The content instance must have a main component.');
-    return;
+  // Swap the components of the swap instances with the target instance's component
+  for (const swapInstance of swapInstances) {
+    if (targetInstance.mainComponent) {
+      swapInstance.swapComponent(targetInstance.mainComponent as ComponentNode);
+    }
   }
-
-  // Swap the components of the nested instances with the content instance's component
-  for (const nestedInstance of nestedInstances) {
-    nestedInstance.swapComponent(contentInstance.mainComponent);
-  }
-
-  figma.closePlugin('Instances swapped successfully.');
 }
 
 // Execute the function
